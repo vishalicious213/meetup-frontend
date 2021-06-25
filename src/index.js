074c4ea73ef513690489f70dc33aaf4f4b0f71a4
vishalicious213 <vishalicious213@gmail.com>
2021-06-24 18:41:43 -0700

    cleaned up code. added API call

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const myElement = <h1>MY ELEMENT</h1>
+
+const restEndpoint = "https://randomuser.me/api"
+
+const callRestApi = async () => {
+  const response = await fetch(restEndpoint)
+  const jsonResponse = await response.json()
+  console.log(jsonResponse)
+}
+
+callRestApi()
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  myElement,
   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
